a387fc69c9a708aa8e0458a6fe0e38acd3db3d26
Dreamzzskiii <boltdreamz@gmail.com>
2024-04-13 09:34:52 +0100

    Initial commit

--- DreamApp/settings.py
+++ DreamApp/settings.py
@@ -8,10 +8,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-&x$i$r6v&jexlck+f#7n)hgqrmbv+3yk(^(%#pp@m#ui0amx4d'
+
+#
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = 'True'
 
 ALLOWED_HOSTS = []
 
