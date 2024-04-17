2e7bb5d4c45c6b3cc35a6518976154762b463754
Dreamzzskiii <boltdreamz@gmail.com>
2024-04-17 21:04:11 +0100

    made changes

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -15,7 +15,7 @@ self.addEventListener('install', function(event) {
 event.waitUntil(
 	caches.open(staticCacheName).then(function(cache) {
 	return cache.addAll([
-		'/',
+		'',
 	]);
 	})
 );
@@ -25,7 +25,7 @@ self.addEventListener('fetch', function(event) {
 var requestUrl = new URL(event.request.url);
 	if (requestUrl.origin === location.origin) {
 	if ((requestUrl.pathname === '/')) {
-		event.respondWith(caches.match('/'));
+		event.respondWith(caches.match(''));
 		return;
 	}
 	}
